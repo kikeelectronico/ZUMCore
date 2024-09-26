991c02c8cd1e814a8aba7611c69ee952962b2a02
Enrique <enrique.gomez.electronico@gmail.com>
2024-09-26 19:11:20 +0200

    update target

--- source/avr/libraries/SPI/SPISettings.h
+++ source/avr/libraries/SPI/SPISettings.h
@@ -69,18 +69,18 @@
   #define SPI_AVR_EIMSK  GIMSK
 #endif
 
-// Bit values are identical for SPI0 and SPI1.
-#define SPR0  SPR00
-#define SPR1  SPR01
-#define CPHA  CPHA0
-#define CPOL  CPOL0
-#define MSTR  MSTR0
-#define DORD  DORD0
-#define SPE  SPE0
-#define SPIE  SPIE0
-#define SPI2X  SPI2X0
-#define WCOL  WCOL0
-#define SPIF  SPIF0
+// // Bit values are identical for SPI0 and SPI1.
+// #define SPR0  SPR00
+// #define SPR1  SPR01
+// #define CPHA  CPHA0
+// #define CPOL  CPOL0
+// #define MSTR  MSTR0
+// #define DORD  DORD0
+// #define SPE  SPE0
+// #define SPIE  SPIE0
+// #define SPI2X  SPI2X0
+// #define WCOL  WCOL0
+// #define SPIF  SPIF0
 
 
 class SPISettings {
